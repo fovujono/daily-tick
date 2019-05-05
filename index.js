886093999d250e67479d0f196ffa5e345c45fb4d
smoot <smoothtacomann@gmail.com>
2019-05-04 23:42:42 -0400

    added new menu item for set timer, and loads html files for menu items

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const url = require("url");
 const path = require("path");
 
 //grabbing from electron
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let setTimeWindow;
 
 // Listen for app to ready
 app.on("ready", function() {
@@ -19,4 +20,51 @@ app.on("ready", function() {
       slashes: true
     })
   );
+
+  //Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  //Insert Menu
+  Menu.setApplicationMenu(mainMenu);
 });
+
+
+ //Handle add window
+function createSetTimeWindow() {
+  //Create New Window
+  addWindow = new BrowserWindow({
+    height: 200,
+    width: 300,
+    title: "Set Timer"
+  });
+  //Load html into window
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "setTimerWindow.html"),
+      protcol: "file:",
+      slashes: true
+    })
+  );
+}
+
+//Create menu template
+const mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Set Time",
+        click() {
+          createSetTimeWindow();
+        }
+      },
+      {
+        label: "Exit",
+        //adding shortcut depending on OS
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
